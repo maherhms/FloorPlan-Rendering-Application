fa8f875967f714d999a8fa54af1fcd440a24c27c
maherhms <maher.3000@outlook.fr>
2026-04-02 08:15:59 +0200

    implement vercel analytics

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
   ScrollRestoration,
 } from "react-router";
 
+import { Analytics } from '@vercel/analytics/next';
 import type { Route } from "./+types/root";
 import "./app.css";
 import "react-toastify/dist/ReactToastify.css";
@@ -96,6 +97,7 @@ export default function App() {
               context={{...authState, refreshAuth,signIn,signOut}}
           />
         </main>
+        <Analytics />
       </>
   )
 }
